e199ae1226a971fb5cdd7cf545f23f515f31022b
Leabs <stevenleabo@gmail.com>
2023-03-20 15:27:34 -0400

    gotta import up above

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,24 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
+import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), react()],
+  integrations: [
+    image(),
+    react(),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: "git-gateway",
+          branch: "main",
+        },
+        collections: [
+          // Content collections
+        ],
+      },
+    }),
+  ],
 });
